3d3cc2707e44785256e1990f0512bdf12de66194
tantrianiherawati-sudo <tantriani.herawati@gmail.com>
2025-09-16 12:30:12 +0700

    Tambah contoh tipe data integer

---
+++ tipe_data_integer.php
@@ -0,0 +1,12 @@
+<?php
+
+echo "Decimal: ";
+var_dump(1234);
+echo "Octal: ";
+var_dump(0123);
+echo "Hexadecimal: ";
+var_dump(8x1A);
+echo "Binary:
+var_dump(8b11111111);
+echo "Underscore di Number:";
+var_dump (1 234_567)
