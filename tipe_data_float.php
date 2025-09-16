1a6947b883460d94e11734b5fb4924323fa27e84
tantrianiherawati-sudo <tantriani.herawati@gmail.com>
2025-09-16 12:35:34 +0700

    Tambah contoh tipe data float

---
+++ tipe_data_float.php
@@ -0,0 +1,10 @@
+<?php
+
+echo "Floating Point: ";
+var_dump(1.234);
+echo "Floating Point dengan E notation Plus (1.2 x 1808): ";
+var_dump(1.2e3);
+echo "Floating Point dengan E notation Minu (7 x 8.881): ";
+var_dump(7e-3);
+echo "Underscore di Floating Point: ";
+var_dump(1_234.567);
