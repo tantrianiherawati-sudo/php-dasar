72d73513828856b1368ddb33a8113c5e6cec0724
tantrianiherawati-sudo <tantriani.herawati@gmail.com>
2025-09-16 13:06:56 +0700

    Tambah data integer

--- tipe_data_integer.php
+++ tipe_data_integer.php
@@ -2,11 +2,17 @@
 
 echo "Decimal: ";
 var_dump(1234);
+
 echo "Octal: ";
 var_dump(0123);
+
 echo "Hexadecimal: ";
-var_dump(8x1A);
-echo "Binary:
-var_dump(8b11111111);
-echo "Underscore di Number:";
-var_dump (1 234_567)
+var_dump(0x1A);
+
+echo "Binary: ";
+var_dump(0b11111111);
+
+echo "Underscore di Number: ";
+var_dump(1_234_567);
+
+?>
